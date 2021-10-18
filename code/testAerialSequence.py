102d3efb454c27387aeedfee5857ed3fa7f209e5
Jae_PC <jaekyuns@andrew.cmu.edu>
2021-10-18 18:31:29 -0400

    Used optional arguments to SDM; tuned tolerance

--- code/testAerialSequence.py
+++ code/testAerialSequence.py
@@ -10,7 +10,7 @@ import time
 parser = argparse.ArgumentParser()
 parser.add_argument('--num_iters', type=int, default=1e3, help='number of iterations of Lucas-Kanade')
 parser.add_argument('--threshold', type=float, default=1e-2, help='dp threshold of Lucas-Kanade for terminating optimization')
-parser.add_argument('--tolerance', type=float, default=0.2, help='binary threshold of intensity difference when computing the mask')
+parser.add_argument('--tolerance', type=float, default=0.1, help='binary threshold of intensity difference when computing the mask')
 args = parser.parse_args()
 num_iters = args.num_iters
 threshold = args.threshold
@@ -23,7 +23,7 @@ for frame in frames_of_interest:
     img1 = seq[:,:,frame-1]
     img2 = seq[:,:,frame]
     ti = time.time()
-    mask = SubtractDominantMotion(img1, img2, threshold, num_iters, tolerance)
+    mask = SubtractDominantMotion(img1, img2, threshold, num_iters, tolerance, 2, 1)
     print('Elapsed time: ', time.time()-ti)
     cur_frame = cv2.cvtColor(np.floor(255*img2).astype('uint8'),cv2.COLOR_GRAY2BGR)
     cur_frame[mask] = [0,0,255]
